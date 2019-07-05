b03e545cbda5a5049b039bfa529396cbb1ae879f
ByakuyaKu <dngr98@mail.ru>
2019-07-05 19:10:56 +0400

    complited 1.8

--- Task01/1.8 NO POSITIVE/Program.cs
+++ Task01/1.8 NO POSITIVE/Program.cs
@@ -10,17 +10,37 @@ namespace ARRAY_PROCESSING
         {
             private static void Main(string[] args)
             {
-                List<int> Random_array = ARRAY_PROCESSING.Random_Array();
-                ARRAY_PROCESSING.Print(Random_array);
+                int[,,] Array = No_Positive();
+                Print_array(Array);
+                Print_array(Swap_Zero(Array));
+                Console.ReadKey();
             }
 
-            static List<int> No_Positive(List<int> arr)
+            static int[,,] Swap_Zero(int[,,] Arr)
             {
-                for (int i = 0; i < arr.Count; i++)
-                {
-                    if
-                }
-                return arr;
+                for (int i = 0; i < Arr.GetLength(2); i++)
+                    for (int j = 0; j < Arr.GetLength(2); j++)
+                        for (int k = 0; k < Arr.GetLength(2); k++)
+                            if (Arr[i, j, k] > 0)
+                                Arr[i, j, k] = 0;
+                            return Arr;
+            }
+
+            static void Print_array(int[,,] Arr)
+            {
+                for (int i = 0; i < Arr.GetLength(2); i++)
+                    for (int j = 0; j < Arr.GetLength(2); j++)
+                        for (int k = 0; k < Arr.GetLength(2); k++)
+                            Console.Write(Arr[i, j, k] + " ");
+                Console.WriteLine();
+            }
+
+            static int[,,] No_Positive()
+            {
+                int[,,] Arr;
+                Arr = new int[2, 2, 2] { { { 0, 1}, { 3, 5} },
+                                           { {-5, 12}, {-6, 10} } };
+                return Arr;
             }
         }
     }
