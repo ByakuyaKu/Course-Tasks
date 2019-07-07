a6d955a83d7adda55f59a2a4894229eb43941de2
ByakuyaKu <dngr98@mail.ru>
2019-07-08 00:59:05 +0400

    complited 1.12

--- Task01/1.12 CHAR DOUBLER/Program.cs
+++ Task01/1.12 CHAR DOUBLER/Program.cs
@@ -6,7 +6,27 @@ namespace _1._12_CHAR_DOUBLER
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter firs string");
+            string inputstr1 = Console.ReadLine();
+            Console.WriteLine("Enter second string (doubler)");
+            string inputstr2 = Console.ReadLine();
+            Char_Doubler(inputstr1, inputstr2);
+            Console.ReadKey();
+        }
+
+        static void Char_Doubler(string s1, string s2)
+        {
+            string output = "";
+            for (int i = 0; i < s2.Length; i++)
+            {
+                for (int j = 0; j < s1.Length; j++)
+                {
+                    if (s1[j] == s2[i])
+                        output = s1.Insert(j, s2[i].ToString()); 
+                }
+                s1 = output;
+            }
+            Console.WriteLine(output);
         }
     }
 }
