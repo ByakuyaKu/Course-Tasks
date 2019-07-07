9b34421ef945b4d8bce2b3382fc39c9e1f847b03
ByakuyaKu <dngr98@mail.ru>
2019-07-07 23:20:06 +0400

    complited 1.11

---
+++ Task01/1.11 AVERAGE STRING LENGTH/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._11_AVERAGE_STRING_LENGTH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter words");
+            string inputstr = Console.ReadLine();
+            StringSplit(inputstr);
+            Console.ReadKey();
+        }
+
+        static void StringSplit(string s)
+        {
+            double count = 0;
+            List<string> elements = new List<string> (s.Split(' ', ',', '.', ':', ';', '\t', '!', '?'));
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (elements[i] == "")
+                {
+                    elements.RemoveAt(i);
+                }
+                count += elements[i].Length;
+                Console.Write(elements[i] + " ");
+            }
+            Console.WriteLine("\n" + "Averege length of inserted words is " + count/elements.Count);
+        }
+    }
+}
