e56ec202e458d3406e35ef296f80fdb5ff21264c
Toga <rodionphilippov@gmail.com>
2019-08-13 19:10:00 +0300

    (~_~)

---
+++ Task5.2/Task5.2/ConsoleView.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task5._2
+{
+    public class ConsoleView
+    {
+        private static void RunProgram()
+        {
+            FileWatcher.DeleteFiles();
+
+            Console.WriteLine("Watching...");
+            Console.WriteLine("Press 'q' to stop watching mode.");
+
+            FileWatcher.Run();
+        }
+
+        public static void ProgramInterface()
+        {
+            for(; ; )
+            {
+                Console.Clear();
+                Console.WriteLine("Please choose mode:"
+                                  + Environment.NewLine + "\t 1: watcher mode"
+                                  + Environment.NewLine + "\t 2: backup mode");
+                if (int.TryParse(Console.ReadLine(), out int result))
+                {
+                    switch (result)
+                    {
+                        case 1:
+                            {
+                                RunProgram();
+                                break;
+                            }
+
+                        case 2:
+                            {
+                                Console.WriteLine("Input file name for back up without .txt");
+                                string fname = BackUp.ChooseFile();
+
+                                Console.WriteLine($"Input date of {fname} for back up and Press enter twice I dont know why (T_T)");
+                                string date = BackUp.ChooseDate(fname);
+
+                                BackUp.GoToVersion(date + fname + ".txt");
+                                Console.WriteLine($"file {fname} backuped");
+                                Console.WriteLine("Press any key to continue");
+                                Console.ReadKey();
+                                break;
+                            }
+
+                        default:
+                            {
+                                Console.WriteLine("Unknown command!");
+                                break;
+                            }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Task5.2/Task5.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleView.ProgramInterface();
+        }
+    }
+}
