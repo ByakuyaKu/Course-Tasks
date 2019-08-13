969125da05d88cd6e881107508ff8f90ce5c54cc
Toga <rodionphilippov@gmail.com>
2019-08-13 18:03:28 +0300

    (~_~)

---
+++ Task5.2/Task5.2/BackUp.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Task5._2
+{
+    public class BackUp
+    {
+        static string sourceDir = Directory.GetCurrentDirectory() + "/Watching";
+        static string backupDir = Directory.GetCurrentDirectory() + "/Backups";
+
+        public static void CopyFile(string name)
+        {
+            try
+            { 
+                string thisDateTime = DateTime.Now.ToString("yyy.MM.dd-HH.mm.ss ");
+
+                string newfname = thisDateTime + name;
+                try
+                {
+                    File.Copy(Path.Combine(sourceDir, name), Path.Combine(backupDir, newfname));
+                    PrintInfoAboutBackupedFile(name, thisDateTime);
+                }
+                catch (IOException copyError)
+                {
+                    Console.WriteLine(copyError.Message);
+                }
+            }
+
+            catch (DirectoryNotFoundException dirNotFound)
+            {
+                Console.WriteLine(dirNotFound.Message);
+            }
+        }
+
+        private static void PrintInfoAboutBackupedFile(string fileName, string dateTime) =>
+            Console.WriteLine($"File {fileName} is rollback to {dateTime}");
+
+        public static string ChooseFile()
+        {
+            string[] txtList = Directory.GetFiles(sourceDir, "*.txt");
+
+            foreach (var file in txtList)
+                Console.WriteLine(file);
+
+            string filename = Console.ReadLine();
+
+            return filename;
+        }
+
+        public static string ChooseDate(string filename)
+        {
+            
+            string[] txtList = Directory.GetFiles(backupDir, "*" + filename + ".txt");
+
+            foreach (var file in txtList)
+                Console.WriteLine(file);
+
+            string date = Console.ReadLine();
+            if (DateTime.TryParse(Console.ReadLine(), out var dateTime))
+                date = dateTime.ToString("yyyy.MM.dd-HH.mm.ss");
+            else
+                new ArgumentException("Wrong date format!");
+
+            return date + " ";
+        }
+
+        public static void GoToVersion(string filename)
+        {
+            string[] files = Directory.GetFiles(backupDir, filename);
+            string name = filename.Substring(20);
+            File.Copy(Path.Combine(backupDir, filename), Path.Combine(sourceDir, name), true);      
+        }
+
+        
+    }
+}
---
+++ Task5.2/Task5.2/FileWatcher.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Task5._2
+{
+    public class FileWatcher
+    {
+        public static void DeleteFiles()
+        {
+            string[] OldBackups = Directory.GetFiles(Directory.GetCurrentDirectory() + "/Backups", "*.txt");
+            foreach (string file in OldBackups)
+                File.Delete(file);
+        }
+
+        public static void Run()
+        {
+            using (FileSystemWatcher watcher = new FileSystemWatcher())
+            {
+                watcher.Path = Directory.GetCurrentDirectory() + "/Watching";
+
+                watcher.NotifyFilter = NotifyFilters.LastAccess
+                                                | NotifyFilters.LastWrite
+                                                | NotifyFilters.FileName
+                                                | NotifyFilters.DirectoryName;
+
+                watcher.Filter = "*.txt";
+
+                watcher.Changed += OnChanged;
+                watcher.Created += OnChanged;
+                watcher.Deleted += OnChanged;
+                watcher.Renamed += OnRenamed;
+
+                watcher.EnableRaisingEvents = true;
+
+                while (Console.Read() != 'q') ;
+            }
+        }
+
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"File: {e.Name} {e.ChangeType}");
+            BackUp.CopyFile(e.Name);
+        }
+
+        private static void OnRenamed(object source, RenamedEventArgs e) =>
+            Console.WriteLine($"File: {e.OldName} renamed to {e.Name}"); 
+    }
+}
